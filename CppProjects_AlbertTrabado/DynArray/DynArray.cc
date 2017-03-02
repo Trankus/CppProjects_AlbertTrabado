4e63faa71b85ea18c1db2669e3c387773055976d
alberttrabadoguiral <alberttrabadoguiral@enti.cat>
2017-03-02 13:58:35 +0100

    final

--- CppProjects_AlbertTrabado/DynArray/DynArray.cc
+++ CppProjects_AlbertTrabado/DynArray/DynArray.cc
@@ -1,19 +1,48 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-//#include "DynArray.hh"
+#include "DynArray.hh"
 
-class DynArray
+
+DynArray::DynArray() :
+		m_capacity(DYN_ARRAY_DEFAULT_SIZE),
+		m_size(0),
+		m_data(new int[DYN_ARRAY_DEFAULT_SIZE]) {};
+
+DynArray::DynArray(size_t size) :
+		m_capacity(size),
+		m_size(0),
+		m_data(new int[size]) {};
+
+/*DynArray::DynArray(size_t size, const int &value) :
+		m_capacity(size),
+		m_size(size),
+		m_data(fill(int *first, int *last, int value)) {};*/
+	
+DynArray::DynArray(int *arr, size_t size):
+		m_capacity(size),
+		m_size(size),
+		m_data(arr) {};
+
+DynArray::~DynArray()
 {
-public:
+	delete m_data;
+	m_data = nullptr;
+}
 
-private:
-	// static constants
-	static const size_t DYN_ARRAY_DEFAULT_SIZE{ 8 };					// initial constant size of the array memory
-	static const size_t DYN_ARRAY_MAX_SIZE{ 1073741823 };			// maximum constant size that the array memory can hold
+DynArray& DynArray::operator= (const DynArray &x)
+{
+	DynArray arr1;
+	arr1.m_data = x.m_data;
+	return arr1;
+}
 
-	// attributes
-	size_t	m_capacity;													// actual size of the allocated memory
-	size_t  m_size;														// current number of elements in the array
-	int	   *m_data;														// pointer to the memory allocated in the Heap
+void DynArray::fill(int *first, int *last, int value)
+{
+
+};
+
+void DynArray::copy(int *first, int *last, int *dest)
+{
+
 };
